211a7a10d021b487f59dcb5ba10b5a899eba4e20
jianyuelin <jianyuelin@westwin.com>
2021-08-16 16:44:41 +0800

    update main func

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use crate::model::{iprovider, ireader};
+use crate::model::node::Node;
+use crate::provider::physics_file_provider::PhysicsFileProvider;
+use crate::reader::node_reader::U8ByteReader;
+
 mod model;
 mod provider;
 mod reader;
 mod test;
 
-
-
 fn main() {
-    println!("Hello, world!");
+    let mut provider: Box<dyn iprovider::IProvider> = Box::from(PhysicsFileProvider::new("./data/health.txt"));
+    let reader: &dyn ireader::IReader<Node<u8>> = &U8ByteReader::new(provider);
+    let nodes = reader.get_nodes();
 }
