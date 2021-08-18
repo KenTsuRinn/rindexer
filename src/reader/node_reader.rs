b9ee23aa79c19450fc5abd4d70a9c263ab6b1624
jianyuelin <jianyuelin@westwin.com>
2021-08-18 11:25:25 +0800

    do some experiment on lifetime

--- src/reader/node_reader.rs
+++ src/reader/node_reader.rs
@@ -2,30 +2,30 @@ use crate::model::iprovider::IProvider;
 use crate::model::ireader::IReader;
 use crate::model::node::Node;
 
-fn line_process(num: usize, contents: String) -> Vec<Node<u8>> {
+fn line_process<'red>(num: usize, contents: String) -> Vec<Node<u8>> {
     let nodes = contents.split(' ').map(|x| {
         return Node::new(num, x.to_string());
     }).collect();
     nodes
 }
 
-pub struct U8ByteReader {
-    provider: Box<dyn IProvider>,
-    line_hooks: Vec<Box<dyn Fn(usize, String) -> Vec<Node<u8>>>>,
+pub struct U8ByteReader<'red> {
+    provider: &'red dyn IProvider,
+    line_hooks: Vec<&'red dyn Fn(usize, String) -> Vec<Node<u8>>>,
 }
 
-impl U8ByteReader {
-    pub fn new(provider: Box<dyn IProvider>) -> U8ByteReader {
+impl<'red> U8ByteReader<'red> {
+    pub fn new(provider: &'red dyn IProvider) -> U8ByteReader<'red> {
         return U8ByteReader {
             provider,
             line_hooks: vec![
-                Box::from(line_process)
+                &line_process
             ],
         };
     }
 }
 
-impl IReader<Node<u8>> for U8ByteReader {
+impl<'red> IReader<Node<u8>> for U8ByteReader<'red> {
     fn get_nodes(&self) -> Vec<Node<u8>> {
         let contents = self.provider.contents();
         let mut line_counter: usize = 0;
--- src/test/physics_file_reader_test.rs
+++ src/test/physics_file_reader_test.rs
@@ -7,15 +7,15 @@ mod physics_file_reader_test {
 
     #[test]
     fn test_get_line_vec() {
-        let provider: &dyn iprovider::IProvider = &PhysicsFileProvider::new("./data/health.txt".to_string());
+        let provider: &dyn iprovider::IProvider = &PhysicsFileProvider::new("./data/health.txt");
         let contents = provider.contents();
         assert_eq!(contents.is_empty(), false);
     }
 
     #[test]
     fn test_get_nodes() {
-        let provider: Box<dyn iprovider::IProvider> = Box::from(PhysicsFileProvider::new("./data/health.txt".to_string()));
-        let reader: &dyn ireader::IReader<Node<u8>> = &U8ByteReader::new(provider);
+        let provider: PhysicsFileProvider = PhysicsFileProvider::new("./data/health.txt");
+        let reader: &dyn ireader::IReader<Node<u8>> = &U8ByteReader::new(&provider);
         let nodes = reader.get_nodes();
         assert_eq!(nodes.is_empty(), false);
         assert_eq!(nodes[0].line, 0);
